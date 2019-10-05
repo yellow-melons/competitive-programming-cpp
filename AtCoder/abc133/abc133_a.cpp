f7b178a977a49113783853273b779315a9aac709
Bill Li <billli@gmail.com>
2019-10-05 18:38:51 -0400

    added atcoder problems

---
+++ AtCoder/abc133/abc133_a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N,A,B;
+    cin >> N >> A >> B;
+    if (N*A < B) {
+        cout << N*A << endl;
+    } else {
+        cout << B << endl;
+    }
+}

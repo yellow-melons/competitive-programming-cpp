ba3e7f02342ccb2374fb93774daa06e778d075ec
Bill Li <billli@gmail.com>
2021-05-27 09:38:36 -0400

    new problems

---
+++ CSES/twosets.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main() {
+  ll n;
+  cin >> n;
+  ll sum = n*(n+1)/2;
+  vector<int> a, b;
+  if (sum%2==1) {
+    cout << "NO" << endl;
+    return 0;
+  }
+  ll ans = sum / 2;
+  for (int i=n; i>=1; i--) {
+    if (i<=ans) {
+      a.push_back(i);
+      ans-=i;
+    } else {
+      b.push_back(i);
+    }
+  }
+  cout << "YES" << endl;
+  cout << a.size() << endl;
+  for (int i : a) {
+    cout << i << " ";
+  }
+  cout << endl;
+  cout << b.size() << endl;
+  for (int i : b) {
+    cout << i << " ";
+  }
+  cout << endl;
+}

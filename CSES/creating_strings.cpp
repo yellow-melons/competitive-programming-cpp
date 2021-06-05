674cae44e12f1149823dfe249decd789d45127ff
Bill Li <billli@gmail.com>
2021-06-05 11:34:10 -0400

    new problems

--- CSES/creating_strings.cpp
+++ CSES/creating_strings.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 #define ll long long
 
 int main() {
-  cout << " hello man " << endl;
+  string s;
+  cin >> s;
+  sort(s.begin(), s.end());
+  vector<string> ans;
+  do {
+    ans.push_back(s);
+  } while(next_permutation(s.begin(), s.end()));
+  cout << ans.size() << "\n";
+  for(string a : ans) {
+    cout << a << "\n";
+  }
 }
